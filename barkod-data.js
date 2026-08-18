d69e8a2aa7a7c2ae99c857f6a5e5e52369ca328c
bricdocs <bricdocs.web@gmail.com>
2026-08-18 12:52:01 +0300

    Create barkod-data.js with JANNERSTEN_MAP
    Added JANNERSTEN_MAP for tournament card database with various series and values.

---
+++ barkod-data.js
@@ -0,0 +1,36 @@
+// --- Jannersten Canlı Turnuva Kart Veri Tabanı (v7.32) ---
+
+const JANNERSTEN_MAP = {
+    // --- MAÇA SERİSİ ---
+    "0101001": "Maça 4",
+    "1001010": "Maça 4",
+    
+    "0101100": "Maça 5",
+    "0011010": "Maça 5",
+    
+    "0011100": "Maça 6", 
+
+    // --- SİNEK SERİSİ ---
+    "0100111": "Sinek 10",
+    "1110010": "Sinek 10",
+    
+    "0101000": "Sinek 6",
+    "0001010": "Sinek 6",
+    
+    "0100010": "Sinek 5", // Eğik çizgi hatası temizlendi
+
+    // --- KARO SERİSİ ---
+    "0101010": "Karo Kız", 
+    
+    "0101110": "Karo 4",
+    "0111010": "Karo 4",
+
+    // --- KUPA SERİSİ ---
+    "0000101": "Kupa Kız",
+    "1010000": "Kupa Kız",
+    
+    "0111100": "Kupa 9",
+    "0011110": "Kupa 9",
+    
+    "0100011": "Kupa 10" // Kupa 10 için geçici yedek yön kodu açıldı
+};
