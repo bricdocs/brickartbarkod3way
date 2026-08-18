d78a058b73121418be7159820b421521be5bd9fa
bricdocs <bricdocs.web@gmail.com>
2026-08-18 12:57:58 +0300

    Initialize barkod-data.js with card mappings
    Added JANNERSTEN_MAP object with card data.

---
+++ barkod-data.js
@@ -0,0 +1,22 @@
+// --- Jannersten Canlı Turnuva Kart Veri Tabanı (v7.32) ---
+// Sizin bulduğunuz ve onayladığınız orijinal veriler milimetrik olarak korunmuştur.
+
+const JANNERSTEN_MAP = {
+    "0101001": "Maça 4",
+    "1001010": "Maça 4",
+    "0101100": "Maça 5",
+    "0011010": "Maça 5",
+    "0011100": "Maça 6", 
+    "0100111": "Sinek 10",
+    "1110010": "Sinek 10",
+    "0101000": "Sinek 6",
+    "0001010": "Sinek 6",
+    "0100010": "Sinek 5 / Kupa 10", // Sizin yazdığınız orijinal ortak isim
+    "0101010": "Karo Kız", 
+    "0101110": "Karo 4",
+    "0111010": "Karo 4",
+    "0000101": "Kupa Kız",
+    "1010000": "Kupa Kız",
+    "0111100": "Kupa 9",
+    "0011110": "Kupa 9"
+};
